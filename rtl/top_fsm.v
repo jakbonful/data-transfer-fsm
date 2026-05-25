0c8e3f3bacc55c4344acf9b6817b5747848917d6
jakbonful <johnalexbonful@gmail.com>
2026-05-25 15:16:19 +0000

    FSM for Data Transfer

--- rtl/top_fsm.v
+++ rtl/top_fsm.v
@@ -1,142 +1,141 @@
-`timescale 1ns/1us
-module top_fsm (
-    input clk,
-    input rst_n,
-
-    // Ram Inputs
-    input ram_in_w_en,
-    input [4:0] ram_in_addr_wr,
-    input [7:0] ram_in_data_wr,
-
-    // Ram Outputs
-    input [3:0] ram_out_addr_rd,
-    output [15:0] ram_out_data_rd,
-
-    // FSM Control Signals
-    input opmode_in,
-    output reg done_out
+module top_fsm(
+        input clk,
+        input rst_n,
+        // Signals for the input RAM
+		input        ram_in_we,
+		input [4 :0] ram_in_addr_wr, 
+		input [7 :0] ram_in_data_wr,
+        // Signals for the output RAM
+		input [3 :0] ram_out_addr_rd, 
+		output [15:0] ram_out_data_rd,     
+        // FSM control signals
+        input opmode_in,
+        output reg done_out
 );
 
     // instantiate the FSM
-    parameter [3:0] // FSM One Hot 
-                    IDLE = 4'b0001,
-                    READ_BYTE0 = 4'b0010,
-                    READ_BYTE1 = 4'b0100,
-                    WRITE_BYTE12 = 4'b1000;
-    
-    // Logic for State Machine
-    reg [3:0] state;
-    reg [3:0] next_state;
-
-    // used to read from RAM_IN and write in RAM_OUT, sort of a counter that tracks RAM location currently being worked on
+    parameter [3:0] //FSM One Hot coding
+                   IDLE           = 4'b0001,
+                   READ_BYTE0     = 4'b0010,
+	  			   READ_BYTE1     = 4'b0100,
+                   WRITE_BYTE12   = 4'b1000;
+                   
+    // Declare logic for the state machine
+    reg [3:0] state, next_state;
+	
+	// Used to read from RAM_IN and write in RAM_OUT
     reg [4:0] ram_pointer;
-
-    reg [4:0] fsm_mem_in_addr_rd;
+    
+    reg [4:0]   fsm_mem_in_addr_rd;
     wire [7:0] fsm_mem_in_data_rd;
+    reg [7:0]  read_byte0_buffer, read_byte1_buffer;
+    
+    reg [3:0]  fsm_mem_out_addr_wr;
+    reg ram_out_we;
 
-    reg [7:0] read_byte0_buffer;
-    reg [7:0] read_byte1_buffer;
+    // Instantiate the RAM modules
+    ram_dp_async_read 
+        #(.WIDTH(8), .DEPTH(32))
+        RAM_IN
+        (.clk    (clk), 
+		 .we     (ram_in_we), 
+		 .addr_wr(ram_in_addr_wr), 
+		 .data_wr(ram_in_data_wr),
+         .addr_rd(fsm_mem_in_addr_rd),
+		 .data_rd(fsm_mem_in_data_rd)
+		);
 
-    reg [3:0] fsm_mem_out_addr_wr;
-    reg ram_out_w_en;
-
-    // RAM Modules
-    ram_dp_async_read #(
-        .width(8),
-        .depth(32),
-    ) RAM_IN (
-        .clk(clk),
-        .w_en(ram_in_w_en),
-        .addr_wr(ram_in_addr_wr),
-        .data_wr(ram_in_data_wr).
-        .addr_rd(fsm_mem_in_addr_rd),
-        .data_rd(fsm_mem_in_data_rd),
-    );
-
-    ram_dp_async_read #(
-        .width(16),
-        .depth(16)
-    ) RAM_OUT (
-        .clk(clk),
-        .w_en(ram_out_w_en),
-        .addr_wr(fsm_mem_out_addr_wr),
-        .data_wr({read_byte1_buffer,read_byte0_buffer}),
-        .addr_rd(ram_out_addr_rd),
-        .data_rd(ram_out_data_rd)
-    );
+    ram_dp_async_read 
+        #(.WIDTH(16), .DEPTH(16))
+        RAM_OUT
+        (.clk    (clk), 
+		 .we     (ram_out_we), 
+		 .addr_wr(fsm_mem_out_addr_wr), 
+		 .data_wr({read_byte1_buffer, read_byte0_buffer}),
+         .addr_rd(ram_out_addr_rd),
+		 .data_rd(ram_out_data_rd)
+		);
 
 
-    // State Machine
-     always @(*) begin
+    // Create the state machine  
+    // Next state logic
+    always @(*)
+        begin
         next_state = IDLE;
         fsm_mem_in_addr_rd = 0;
-        ram_out_w_en = 0;
-        case (state)
-                IDLE : begin
-                    if (opmode_in == 1'b1)
-                        next_state =  READ_BYTE0;
-                end
-                READ_BYTE0 : begin
-                    fsm_mem_in_addr_rd = ram_pointer;
-                    next_state = READ_BYTE1;
-                end
-                READ_BYTE1 : begin
-                    fsm_mem_in_addr_rd = ram_pointer;
-                    next_state = WRITE_BYTE12;
-                end
-                WRITE_BYTE : begin
-                    if (done_out == 1) begin
-                        next_state = IDLE;
-                    end else begin
-                        next_state = READ_BYTE;
-                    end
-                    ram_out_w_en;
-                end
-            default: begin
-                next_state = IDLE;
-            end
+        ram_out_we = 0;
+        case(state) //
+			IDLE        :begin
+						    if (opmode_in == 1'b1)
+						        next_state = READ_BYTE0;
+						end
+			READ_BYTE0  :begin
+							fsm_mem_in_addr_rd = ram_pointer;
+							next_state = READ_BYTE1;
+						end   
+			READ_BYTE1  :begin
+							fsm_mem_in_addr_rd = ram_pointer;
+							next_state = WRITE_BYTE12;
+						end 			   
+			WRITE_BYTE12: begin
+							if (done_out == 1) begin
+								next_state = IDLE;
+							end else begin
+								next_state = READ_BYTE0;                     
+							end
+							ram_out_we = 1;
+						end     
+			default: begin 
+					   next_state = IDLE;
+					end
         endcase
-     end
+    end
 
-    // Sequencer Logic
-     always @(posedge clk or negedge rst_n) begin
-        if (!rst_n)
-            state <= IDLE;
-        else
-            state <= next_state;
-     end
-
-    // Counter Logic
-    always @(posedge clk or negedge rst_n) begin
+    //State sequencer logic
+	always @(posedge clk or negedge rst_n)
+	begin
+		if(!rst_n)
+		    state <= IDLE;
+		else
+		    state <= next_state;
+	end    
+    
+    //Declare internal counter logic 
+    always @(posedge clk or negedge rst_n)
+    begin
         if (!rst_n)
             ram_pointer <= 0;
         else if ((state == READ_BYTE0) || (state == READ_BYTE1))
-            ram_pointer <= ram_pointer = 1'b1;
+            ram_pointer <= ram_pointer + 1'b1;
     end
 
-    always @(posedge clk or negedge rst_n) begin
+   always @(posedge clk or negedge rst_n)
+    begin
         if (!rst_n)
             fsm_mem_out_addr_wr <= 0;
         else if (state == READ_BYTE0)
-            fsm_mem_out_addr_wr <= (ram_pointer >> 1);
-    end
+		    fsm_mem_out_addr_wr <= (ram_pointer >> 1);
+    end 
 
-    always @(posedge clk or negedge rst_n) begin
-        if (!rst_n)
+    always @(posedge clk or negedge rst_n)
+    begin
+        if(!rst_n)
             done_out <= 0;
-        else if   (opmode == 1)
-            done_out <= 0;
+	    else if (opmode_in == 1)
+		    done_out <= 0;
         else if (ram_pointer == 5'd31)
             done_out <= 1;
     end
 
-    always @(posedge clk or negedge rst_n) begin
-        if (!rst_n)begin
+    always @(posedge clk or negedge rst_n)
+    begin
+        if(!rst_n) begin
             read_byte0_buffer <= 0;
-            read_byte1_buffer <= 0;
+	        read_byte1_buffer <= 0;
         end else begin
             read_byte0_buffer <= fsm_mem_in_data_rd;
-            read_byte1_buffer <= read_byte0_buffer 
-        end
-    end
+	        read_byte1_buffer <= read_byte0_buffer;
+	    end
+    end 
+ 
 endmodule
